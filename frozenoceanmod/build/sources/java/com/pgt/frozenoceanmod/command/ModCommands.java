a906eb9dc6a375d1cdebbf38338ea2d13307cddf
TheRealPGT <84862651+TheRealPGT@users.noreply.github.com>
2025-09-26 16:15:02 +0000

    Delete frozenoceanmod directory

--- frozenoceanmod/build/sources/java/com/pgt/frozenoceanmod/command/ModCommands.java
+++
@@ -1,9 +0,0 @@
-package com.example.frozenocean.command;
-
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
-
-public class ModCommands {
-    public static void registerCommands(FMLServerStartingEvent event) {
-        event.registerServerCommand(new CommandFindFrozenOcean());
-    }
-}
--- frozenoceanmod/src/main/java/com/pgt/frozenoceanmod/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package com.example.frozenocean;
-
-public class CommonProxy {
-    public void registerRenderers() {
-        // Server side - no rendering
-        System.out.println("CommonProxy registerRenderers called (server side)");
-    }
-}
